96c85ef6ae5cefffd6415a9bdd46cd202dc8150a
dipanshu18 <torawanedipanshu@gmail.com>
2025-02-13 20:01:45 +0530

    material top bar nav

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,38 +1,21 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import LikedPage from "../liked";
+import SuggestedPage from "../suggested";
+import PurchasedPage from "../purchased";
+
+const Tab = createMaterialTopTabNavigator();
+
 export default function ForYouPage() {
   return (
     <View style={{ flex: 1 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          gap: 20,
-          justifyContent: "space-evenly",
-          marginVertical: 10,
-          padding: 10,
-        }}
-      >
-        <Link href={"/suggested"}>
-          <Text>Suggested</Text>
-        </Link>
-        <Link href={"/purchased"}>
-          <Text>Purchased</Text>
-        </Link>
-        <Link href={"/liked"}>
-          <Text>Liked</Text>
-        </Link>
-      </View>
-
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>For you page</Text>
-      </View>
+      <Tab.Navigator>
+        <Tab.Screen name="liked" component={LikedPage} />
+        <Tab.Screen name="suggested" component={SuggestedPage} />
+        <Tab.Screen name="purchased" component={PurchasedPage} />
+      </Tab.Navigator>
     </View>
   );
 }
