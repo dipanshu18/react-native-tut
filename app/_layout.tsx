18927e95d6a3fcdda838a42c6d5c0f9c61e63a55
dipanshu18 <torawanedipanshu@gmail.com>
2025-02-13 18:29:49 +0530

    implemented basic expo file-based routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,27 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/components/useColorScheme';
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link, Slot } from "expo-router";
+import { View, Text } from "react-native";
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <SafeAreaView>
+      <View style={{ height: "90%" }}>
+        <Slot />
+      </View>
+
+      <View style={{ backgroundColor: "pink" }}>
+        <Link href={"/account"}>
+          <Text>Take me to accounts page</Text>
+        </Link>
+
+        <Link href={"/"}>
+          <Text>Take me to for you page</Text>
+        </Link>
+
+        <Link href={"/explore"}>
+          <Text>Take me to explore page</Text>
+        </Link>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function ForYouPage() {
+  return (
+    <View>
+      <Text>For you page</Text>
+    </View>
+  );
+}
